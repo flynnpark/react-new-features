d9758f36c80ad8fe28ae040e022f893927ed8909
Flynn <wphestiraid@gmail.com>
2019-02-07 23:22:22 +0900

    Effect Hook 예시

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import StateHook from './components/StateHook';
+import EffectHook from './components/EffectHook';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <StateHook />
+        <EffectHook />
       </div>
     );
   }
